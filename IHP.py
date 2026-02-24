dca5667ea8709d59ad68da19132bffd3f2a3c40f
Nathaniel Quantock <48071345+nathanielq@users.noreply.github.com>
2026-02-24 14:19:13 -0800

    Add working directory for IHP.py

---
+++ IHP.py
@@ -0,0 +1,269 @@
+# <> Imports <> #
+# - Config - #
+import config
+# - Built In - #
+import os
+import sys
+from datetime import datetime
+# - Third Party - #
+import paramiko
+import base64
+import polars as pl
+from pathlib import Path
+from apiclient import discovery
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from google.oauth2.credentials import Credentials
+import google.oauth2.service_account
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from googleapiclient.errors import HttpError
+
+# <> Logging <> #
+log_file = config.log_file
+sys.stdout = open(config.log_file, 'a')
+sys.stderr = sys.stdin
+cur_time = datetime.now().strftime("%m-%d %H:%M")
+print(f'Starting Flex_IHP at {cur_time}\n')
+
+# <> Generate the necessary authentication for accessing Google Drive using Google API <> #
+def Authenticate():
+    token = config.oauth_token
+    scope = config.oauth_scopes
+    google_creds = config.oauth_creds
+    creds = None
+    if os.path.exists(token):
+        creds = Credentials.from_authorized_user_file(token, scope) #if token.json exists pull credentials from that file to generate creds
+    if not creds or not creds.valid: #if creds were not generated from token, or creds is no longer valid check a few instances of what causes that
+        if creds and creds.expired and creds.refresh_token: #if creds.json exists but is expired refresh the token
+            creds.refresh(Request())
+        else: #if creds is blank token did not have the necessary info from Credentials.json and needs to be generated via below command
+            flow = InstalledAppFlow.from_client_secrets_file(google_creds, scope) #CREDENTIALS is a json file pulled from the project file in Google Cloud Platform
+            creds = flow.run_local_server(port=0) #im not gonna even pretend to fully understand this line :^)
+        with open(token, 'w') as token: #write the newly generated creds to Token.json to speed up this process if call is done wihthin the expiration time
+            token.write(creds.to_json())
+    return creds
+
+# <> Updated oauth2 method of creating service account credentials <> #
+def Get_Service():
+    try:
+        credential = google.oauth2.service_account.Credentials.from_service_account_file(
+        config.service_key, scopes=config.service_scopes, subject=config.gmail_delegated_user_email)
+    except:
+        print(f'Could not generate service credentials')
+    return discovery.build('gmail', 'v1', credentials=credential)
+
+# <> Read the IHP Google Sheet for all values <> #
+def Get_IHP_Data(service):
+    try:
+        print('Getting Data..')
+        results = service.spreadsheets().values().get(
+                spreadsheetId=config.file_id, 
+                range=config.sheet_range).execute()
+        print('Grabbed data')
+        values = results.get("values", [])
+    except Exception as e:
+        print(f'Could not get data: {e}')
+
+    headers = values[0]
+    rows = values[1:]
+    # - Empty cells in GSheets are treated as non existent when reading. 
+    #   the formula below checks each row and pads it with ''values
+    #   until it reaches the same row length as the header length - #
+    width = len(headers)
+    rows = [r + [''] * (width-len(r)) for r in rows]
+    # - Create polars dataframe - #
+    hp_df = pl.DataFrame(rows, schema=headers, orient="row")
+    key_df = Get_Students_From_Activity_File()
+    #Get_Teacher_Emails(service)
+    
+    df = Trim_Data(hp_df, key_df)
+    return df
+
+# <> Function trim down to only necessary data <> #
+def Trim_Data(result, key_df):
+    # - Select Only Necessary columns - #
+    columns = config.columns
+    result = result.select(columns)
+    # - filter to only show non empty health plan notes - #
+    result = result.filter(pl.any_horizontal(pl.col('Health Plan Notes')!='', pl.col('Active Alerts')!=''))
+    # - Join the keys_df to the result_df via matching student ids - #
+    result = result.join_where(key_df, pl.col('Student Number').cast(pl.String) == pl.col('SIS ID').cast(pl.String))
+    # - Drop columns containing student ID (not relevant) - #
+    result = result.drop(['Student Number', 'SIS ID'])
+    class_lists = result.partition_by('Teacher LN')
+    columns.append('Teacher LN')
+    columns.remove('Student Number')
+    return class_lists
+
+def Get_Students_From_Activity_File():
+    # - If testing skip paramiko and return test file df - #
+    if config.test_flag == 'test':
+        print('Running in test mode..')
+         # - Read in csv to pl df - #
+        key_df = pl.read_csv(config.test_download_path, separator=',')
+        # - Select only necessary columns - #
+        key_df = key_df.select(pl.col('SIS ID'), pl.col('Teacher FN'), pl.col('Teacher LN'))
+        return key_df
+    print('Running in Production Mode')
+    # - Get Flex_Attendance_Full from Securly file server - #
+    with paramiko.SSHClient() as ssh:
+        # - Open known hosts file and load that here - #
+        try:
+            ssh.load_host_keys(config.known_hosts)
+        except FileNotFoundError as e:
+            print(f'Could not load host keys file:\n {e}')
+        # - Connect to the sftp server and get the flex_activities file - #
+        try:
+            ssh.connect(username=config.username, hostname=config.host, password=config.password, port=22)
+            with ssh.open_sftp() as sftp:
+                sftp.get(config.get_path, config.download_path)
+                print('Got File from server')
+        except paramiko.ssh_exception.AuthenticationException as e:
+            print(f"Could not Authenticate to server: {e}")
+    
+    # - Read in csv to pl df - #
+    key_df = pl.read_csv(config.download_path, separator=',')
+    # - Select only necessary columns - #
+    key_df = key_df.select(pl.col('SIS ID'), pl.col('Teacher FN'), pl.col('Teacher LN'))
+    return key_df
+
+# <> Use Gmail API for greater security <> #
+def Send_Email(body, teacher_name, teacher_email, email_service):
+    try:
+        # - Create the email message - #
+        message = MIMEMultipart('related')
+        if config.test_flag == 'test':
+            message['To'] = config.test_email 
+        elif config.test_flag == 'prod':
+            message["To"] = teacher_email
+        
+        message['From'] = config.from_email
+        
+        # - if there was an error with the email gathering, send to myself with changed subject line - #
+        if teacher_email == config.error_email:
+            message['Subject'] = f'Could not locate teacher_email for {teacher_name}'
+        # - Else just send message as normal - #
+        else:
+            message['Subject'] = f'Gator Time IHP Reports for {teacher_name}'
+            message['Cc'] = config.cc_emails
+            
+        # - Configure it to accept HTML - #
+        alt = MIMEMultipart("alternative")
+        html_body = MIMEText(body, 'html')
+        alt.attach(html_body)
+        message.attach(alt)
+
+        # - Conver the misd logo png to mimeimage so it can be in the email - #
+        img_bytes = Path("./0.png").read_bytes()
+        img = MIMEImage(img_bytes, _subtype="png")
+        img.add_header("Content-ID","<logo>")
+        img.add_header("Content-Disposition", "inline", filename="0.png")
+        message.attach(img)
+
+        # - Properly encoded message for Gmail (base64) - #
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+        # - raw text for final message - #
+        create_message = {'raw': encoded_message}
+        # - send the actual message currently impersonating my email - #
+        send_message = (
+            email_service.users()
+            .messages()
+            .send(userId='me', body=create_message).execute()
+        )
+    except HttpError as error:
+        print(f'An error Occurred: {error}')
+        send_message = None
+    print(send_message)
+
+# <> Display names, legal names, and emails are not consistent across systems.
+#  Needed a system to match names to emails for teachers. Ended up landing an already automated
+#  staff list getting pulled for our Apple School Manager instance from Qmlativ (Skyward)<> #
+def Get_Teacher_Emails():
+    # - read in as df through polars PL - #
+    staff_df = pl.read_csv(config.staff_file, separator=',')
+    # - Grab necessary columns - #
+    staff_df = staff_df.select(pl.col('first_name'),pl.col('last_name'), pl.col('email_address'))
+    # - Convert columns to list of dicts - #
+    mapping_list = staff_df.to_dicts()
+    # - modify mapping_list to contain a full name key,value pair from first_name last_name values - #
+    for mapping in mapping_list:
+        mapping['full_name'] = mapping['first_name'].lower().strip() + ' ' + mapping['last_name'].lower().strip()
+    return mapping_list
+        
+# <> Put email for each teacher together before being sent <> #
+def Build_Email_Template(class_dfs):
+    # - Generate the Credentials for admin access
+    email_service = Get_Service()
+    email_list = Get_Teacher_Emails()
+    counts = {'total num': 0, 'matched_emails': 0, 'unmatched_emails': 0}
+    # - The total df is split into individual classes. Iterate through those classes - #
+    for classroom in class_dfs:
+        counts['total num'] += 1
+        body = config.body
+        # - Combine all columns into a single struct in a new column named 'Students' - #
+        classroom = classroom.select(pl.struct(pl.all()).alias('Students'))
+        # - Convert to a python list - #
+        student_list = classroom["Students"].to_list()
+        # - Teacher_name for building contacts - #
+        teacher_name = student_list[0].get('Teacher FN') + ' ' + student_list[0].get('Teacher LN')
+        # - Get the email of the Gator Time Teacher - #
+        teacher_email = next((email.get('email_address') for email in email_list if teacher_name.lower()
+                              .strip() == email.get('full_name').lower().strip()), None)
+        print(teacher_name, teacher_email)
+
+        # - Set teacher_email to myself and change subject if teacher_email == None - #
+        if teacher_email is None:
+            print(f'[Error] Could not get teacher email. Setting it to error_email for error checking..')
+            teacher_email = config.error_email
+            counts['unmatched_emails'] += 1
+        else:
+            counts['matched_emails'] += 1
+
+        # - Styling for rounded edges in first row first cell and last row first cell
+        last_row_style_1 = ""
+        last_row_style_2 = ""
+        # - Build a table from each student in the class being a row - #
+        rows = []
+        for i,student in enumerate(student_list):
+            # - Alternating colors - #
+            if i % 2 == 0:
+                style = "'background-color:#f2f2f2; color: #393e46; padding-top: 5px; padding-bottom: 5px;'"
+            else:
+                style = "'background-color: #393e46; color:#f2f2f2; padding-top: 5px; padding-bottom: 5px;'"
+            # - See above for rounded edges - #
+            if i == len(student_list) - 1:
+                last_row_style_1 = "'border-radius: 0 0 0 10px; padding-left: 5px;"
+                last_row_style_2 = "'border-radius: 0 0 10px 0;'"
+            
+            # - Build the table - #
+            rows.append(f""" 
+            <tr style={style}>
+                <td style={last_row_style_1} padding-left: 5px;'>{student.get('Full Name (LF)')}</td>
+                <td>{student.get("Health Plan Notes")}</td>
+                <td style={last_row_style_2}>{student.get("Active Alerts")}</td>
+            </tr>""")
+        # - add closing tags - #
+        body += ''.join(rows) + """
+                </tbody>
+                </table>
+                </div>
+            </body>
+        </html>"""
+
+        # - format to replace teacher_name variable (it's in config.py) - #
+        body = body.format(teacher_name=teacher_name)
+        # - send completed table to the Email_List function to be sent via API - #
+        Send_Email(body, teacher_name, teacher_email, email_service)
+    print(counts)
+
+# <> Main Calls <> #
+if __name__ == '__main__':
+    sheets_service = build('sheets', 'v4', credentials=Authenticate())
+    class_dfs = Get_IHP_Data(sheets_service)
+    # - Send email to teachers - #
+    Build_Email_Template(class_dfs)
+    print('\nFinished Running Flex_IHP.py\n')
+
---
+++ config.py
@@ -0,0 +1,94 @@
+# <> Flex_IHP Config File <> #
+# - Imports - #
+import keyring
+import os
+# <> Test Flag <> #
+#test_flag = 'test'
+test_flag = 'prod'
+# <> Flex Attendance File <> #
+
+# <> SFTP Connection <> #
+host = 'HOSTNAME'
+username = 'USERNAME'
+# - Password stored via Windows Credential Manager - #
+password = keyring.get_password(username='WINDOWS_CREDENTIALS_USERNAME', service_name='WINDOWS_CREDENTIALS_SERVICE_NAME')
+known_hosts = os.path.join(os.path.expanduser("~"), '.ssh', 'known_hosts')
+
+# - File Paths - #
+local_path = 'NAME_OF_LOCAL_FILE'
+get_path = 'SFTP_FILE_PATH'
+test_download_path = 'LOCAL_PATH_FOR_TESTING'
+staff_file = 'PATH_FOR_TEACHER_EMAILS'
+download_path = 'DOWNLOAD_PATH'
+
+# - Log File - #
+log_file = 'LOG_FILE_PATH'
+
+# <> Google Variables <> #
+# - Google API Scope - #
+oauth_scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file']
+service_scopes = ['https://www.googleapis.com/auth/gmail.send']
+# - Google API Token and Creds file paths - #
+# - Any changes to scope will require deleting creds and then running the auth flow again - #
+# - creds/keys/tokens should be stored in project directory - #
+oauth_token = '\\Flex_IHP\\token.json'
+oauth_creds = '\\Flex_IHP\\credentials.json'
+service_key = '\\Flex_IHP\\service_account_key.json'
+gmail_delegated_user_email = 'EMAIL_TO_SEND_FROM'
+file_id = 'GOOGLE_SHEET_FILE_ID'
+sheet_name = 'SHEET_NAME'
+sheet_range = "SHEET_RANGE"
+
+# - File Variables - #
+columns = ['Student Number','Full Name (LF)', 'Health Plan Notes', 'Active Alerts']
+
+# <> Email Builder <> #
+error_email = 'ERROR_EMAIL'
+test_email = 'TEST_EMAIL'
+from_email = 'FROM_EMAIL'
+cc_emails = 'CC_EMAIL_STRING_NOT_A_LIST'
+# - To prevent errors - #
+teacher_name = ''
+# - Start the email template here. Build in the main python after - #
+body = """
+    <html lang='en-US'>
+    <head>
+            <style>
+            body{{background-color: #f2f2f2; color: #393e46; text-align:center;}}
+            h1{{text-align: center; font-family: Monospace;}}
+            table.table{{
+            margin-top: 0;
+            border: 2px solid black;
+            border-radius: 10px;
+            border-collapse: separate;
+            border-spacing: 0;
+            width: 100%;
+            }}
+
+            th, td{{
+                padding: 8px;
+                vertical-align: top;
+            }}
+            .logo{{vertical-align: bottom;}}
+            .email_body{{align: center;}}
+        </style>
+    </head>
+    <body>
+    <div class = 'email_body'>
+        <h1>
+            <img src="cid:logo" width="50" height="50" class="logo">
+            <strong>Flex Period Student IHPs for {teacher_name}</strong>
+            <img src="cid:logo" width="50" height="50" class="logo"> 
+        </h1>
+        <table class='table' role='presentation'> 
+            <thead>
+                <tr style='background-color: #f95959; font-size: 20px;'>
+                    <th scope="col" style="border-radius: 10px 0 0 0; margin-right: 20px;">Student</th>
+                    <th scope="col">Health Plan Notes</th>
+                    <th scope='col'  style="border-radius: 0 10px 0 0" >Active Alerts</th>
+                </tr>
+            </thead>
+            <tbody>
+            """
